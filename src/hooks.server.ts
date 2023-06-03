8fe8cd7ed2c15b49f904852a83590c7946ce5e6a
MyNameRallybjorn <98455638+Bjofel@users.noreply.github.com>
2023-06-03 20:19:01 +0200

    render compatible v1?

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,5 +13,19 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 
+	if (event.request.method === "OPTIONS") {
+		return new Response(null, {
+			headers: {
+				"Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+				"Access-Control-Allow-Origin": "*",
+			},
+		});
+	}
+
+	const response = await resolve(event);
+	response.headers.append("Access-Control-Allow-Origin", `*`);
+	return response;
+
+
 	return resolve(event);
 };
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 
+const uri = "mongodb+srv://bjofel:<password>@shopkeep.tz9a4su.mongodb.net/?retryWrites=true&w=majority";
 export async function connect(): Promise<MongoClient> {
     return await MongoClient.connect('mongodb://localhost:27017/');
 }
